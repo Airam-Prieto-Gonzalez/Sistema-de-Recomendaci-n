7182e3d100eef67c2026aa33fb56075e0a6dd11b
4lbxrtz <4lbxrtz@gmail.com>
2025-10-14 10:18:33 +0100

    chore: add some `utility matrix` examples

---
+++ src/main.py
@@ -0,0 +1,27 @@
+from cli import parse_args
+from fill import fill_matrix
+from utils import print_matrix, read_matrix
+
+
+def main():
+    args = parse_args()
+
+    matrix, min_score, max_score = read_matrix(args.file)
+    filled_matrix, sim_matrix, neighbors_selected = fill_matrix(
+        matrix,
+        k=args.neighbors,
+        metric=args.metric,
+        method=args.type,
+    )
+
+    print("\nMatriz de utilidad completada:")
+    print_matrix(filled_matrix)
+
+    print("\nMatriz de similitudes:")
+    print_matrix(sim_matrix)
+
+    
+
+
+if __name__ == "__main__":
+    main()
